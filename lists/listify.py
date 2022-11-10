2134401417f53a40e79e46d6628a382335791bfd
karan pratap shaw <91419527+iCaran@users.noreply.github.com>
2022-11-10 09:05:09 -0800

    Update listify.py

--- lists/listify.py
+++ lists/listify.py
@@ -1,11 +1,11 @@
 data = []
 
-with open('remove_miui.txt', 'r+') as file:
+with open('remove_xyz.txt', 'r+') as file:
 	for each in file:
 		s=each.strip()
 		x=s.find('#')
 		if s!='' and x!=0:
 			data.append(s[:x-1]+'\n')
 
-with open('miui.txt', 'w') as file:
+with open('xyz.txt', 'w') as file:
 	file.writelines(data)
